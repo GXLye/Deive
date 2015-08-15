cf9fbafbc864344987781ba091c62ede280209ce
Lye Guang Xing <gxlye@me.com>
2015-08-16 04:29:50 +0800

    Added Parse Framework & some storyboard setups

--- Deive/AppDelegate.swift
+++ Deive/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("0FiLLAR7K5Qck4Y9mVnEPx2LR1njiYoxzFjCxO5G",
+            clientKey: "BPWAY1N1MU7APzeOamsMolM2QDMJwQSTkjRFT8Ns")
+        
+        // Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
